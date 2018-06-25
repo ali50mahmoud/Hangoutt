f495df823aebcad5c38e3e3d3b7bf10315fb5813
ali50mahmoud <ali50mahmoud@yahoo.com>
2018-06-25 02:45:06 +0200

    Initial commit

---
+++ app/src/main/java/com/live/hangout/MessageResponse.java
@@ -0,0 +1,16 @@
+package com.live.hangout;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by amogh on 8/6/17.
+ */
+
+public class MessageResponse {
+    @SerializedName("affected_rows")
+    Integer affectedRows;
+
+    public Integer getAffectedRows(){
+        return affectedRows;
+    }
+}
